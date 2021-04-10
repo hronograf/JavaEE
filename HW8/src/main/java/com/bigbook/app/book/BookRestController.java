9ee9b120b3112c444b7fef7919f48e7f34c772d2
hronograf <vlad.boholii@gmail.com>
2021-04-10 18:33:17 +0300

    add adding/deleting from favourites

---
+++ HW8/src/main/java/com/bigbook/app/book/BookRestController.java
@@ -0,0 +1,52 @@
+package com.bigbook.app.book;
+
+import com.bigbook.app.book.dto.BookInfoDto;
+import com.bigbook.app.book.dto.ChangeFavouritesRequestDto;
+import com.bigbook.app.book.dto.SearchBookResponseDto;
+import com.bigbook.app.utils.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Set;
+
+@RequiredArgsConstructor
+
+@RestController
+@RequestMapping("/books")
+public class BookRestController {
+
+    private final BookRepository bookRepository;
+    private final BookService bookService;
+
+    @PostMapping("/create")
+    public Response<Void> createBookPost(@RequestBody BookEntity bookEntity) {
+        bookService.saveBook(bookEntity);
+        return Response.of("Successfully added", null);
+    }
+
+    @GetMapping("/search")
+    public Response<SearchBookResponseDto> getFilteredByTitleBooks(
+            @RequestParam(value = "title", defaultValue = "") String title,
+            @RequestParam(value = "isbn", defaultValue = "") String isbn,
+            @RequestParam(value = "page", defaultValue = "0") int pageNumber
+    ) {
+        return Response.success(bookService.findBooks(title, isbn, pageNumber));
+    }
+
+    @PostMapping("/add-to-favourites")
+    public void addToFavourites(@Valid @RequestBody ChangeFavouritesRequestDto requestDto) {
+        bookService.addToFavourites(requestDto);
+    }
+
+    @PostMapping("/delete-from-favourites")
+    public void deleteFromFavourites(@Valid @RequestBody ChangeFavouritesRequestDto requestDto) {
+        bookService.deleteFromFavourites(requestDto);
+    }
+
+    @GetMapping("/favourite-info")
+    public Response<Set<BookInfoDto>> getFavouriteBooks() {
+        return Response.success(bookService.getFavouriteBooks());
+    }
+
+}
